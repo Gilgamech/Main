336325143d147afe306e3595453e6567062978c9
Stephenson Empetheez Gilgamech <stephengillie@gmail.com>
2016-07-05 07:59:54 -0700

    FizzBuzz demo
    PowerGIL command to make this:
    
    New-Module FizzBuzz.ps1 (New-Function Filter-FizzBuzz (New-FunctionStatement If  -not _ Modulus 15 'return "FizzBuzz"' (New-FunctionStatement ElseIf  -not _ Modulus 5 'return "Fizz"' (New-FunctionStatement ElseIf  -not _ Modulus 3 'return "Buzz"' 'return $_' ) ) ) -Filter)

---
+++ FizzBuzz.ps1
@@ -0,0 +1,26 @@
+# FizzBuzz.ps1 Build: 0 2016-07-05T07:55:40 Filter Filter-FizzBuzz {
+# Update Path: C:\Dropbox\FizzBuzz.ps1
+# Build : LineNo : Update Notes
+# 
+# 
+# 
+# 
+# 0 : 0 : # FizzBuzz.ps1 Build: 0 2016-07-05T07:55:40 Filter Filter-FizzBuzz {
+	
+	If (!($_ % 15)) {
+		return "FizzBuzz"
+	} ElseIf (!($_ % 5)) {
+	 	return "Fizz"
+	 } ElseIf (!($_ % 3)) {
+	  	return "Buzz"
+	  } else {
+	  	return $_
+	  
+	 
+	}; #end If _
+	
+
+}; #end Filter-FizzBuzz # Update Path: FizzBuzz.ps1 # Build : LineNo : Update Notes
+$FizzBuzz = 'C:\Dropbox\FizzBuzz.ps1'
+Write-Host -f green "C:\Dropbox\FizzBuzz.ps1 build: $((gc $FizzBuzz)[0].split(' ')[3] )"
+
